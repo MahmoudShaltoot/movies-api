85d6f9a60c24ecf19e244f82e50d498b23e84a95
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-24 01:00:21 +0200

    integrate with tmdb /genre api to return movies genres

--- src/tmdb/tmdb.service.ts
+++ src/tmdb/tmdb.service.ts
@@ -5,16 +5,24 @@ import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class TmdbService {
-    private readonly apiUrl;
-    private readonly apiKey;
+  private readonly apiUrl;
+  private readonly apiKey;
 
-    constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {
-        this.apiUrl = configService.get<string>('TMDB_API_URL');
-        this.apiKey = configService.get<string>('TMDB_API_KEY');
-    }
+  constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {
+    this.apiUrl = configService.get<string>('TMDB_API_URL');
+    this.apiKey = configService.get<string>('TMDB_API_KEY');
+  }
 
-    async getPopularMovies(page: number = 1) {
-        const url = `${this.apiUrl}/discover/movie?api_key=${this.apiKey}&page=${page}&sort_by=popularity.desc`;
-            return (await firstValueFrom(this.httpService.get(url))).data;
-    }
+  async getPopularMovies(page: number = 1) {
+    const url = `${this.apiUrl}/discover/movie?api_key=${this.apiKey}&page=${page}&sort_by=popularity.desc`;
+    return (await firstValueFrom(this.httpService.get(url))).data;
+  }
+
+  async getGenres() {
+    return (
+      await firstValueFrom(
+        this.httpService.get(`${this.apiUrl}/genre/movie/list?language=en`, {
+          params: { api_key: this.apiKey },
+        }))).data.genres
+  }
 }
