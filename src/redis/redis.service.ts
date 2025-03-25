ff69ac70bd93db3d0dd54e5731732d29e3c5baaf
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-25 23:40:35 +0200

    implement redis setHashkey & getHashkEY to cache movies data

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -15,6 +15,24 @@ export class RedisService {
       db: this.configService.get<number>('REDIS_DATABASE_INDEX')
     });
   }
+
+  async setHashKey(key: string, value: { id: number, external_id: number; title: string; original_title: string; poster: string; genres: string[] }): Promise<void> {
+    await this.redis.hmset(key, {
+      external_id: value.external_id,
+      title: value.title,
+      original_title: value.original_title,
+      poster: value.poster,
+      genres: JSON.stringify(value.genres),
+    });
+  }
+
+  async getHashKey(key: string): Promise<{ [key: string]: string }> {
+    const result = await this.redis.hgetall(key);
+    if (result.genres) {
+      result.genres = JSON.parse(result.genres);
+    }
+    return result;
+  }
 }
 
 
