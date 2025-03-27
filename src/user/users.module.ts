badb7cf31e0333c743cacaa6f226c44680a050d3
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-28 01:29:58 +0200

    use plural naming for users module

--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './user.service';
-import { UsersController } from './user.controller';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
