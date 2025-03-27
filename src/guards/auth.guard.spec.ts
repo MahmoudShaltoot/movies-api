69a4a1dbe6882eff7bb83cbaa88472565af50eac
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-28 01:21:50 +0200

    fix guards test cases

--- src/guards/auth.guard.spec.ts
+++ src/guards/auth.guard.spec.ts
@@ -1,7 +1,29 @@
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let jwtService: JwtService;
+
+  // Mock JwtService
+  const mockJwtService = {
+    verify: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: JwtService, useValue: mockJwtService },
+      ],
+    }).compile();
+
+    guard = module.get<AuthGuard>(AuthGuard);
+    jwtService = module.get<JwtService>(JwtService);
+  });
+
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(guard).toBeDefined();
   });
 });
