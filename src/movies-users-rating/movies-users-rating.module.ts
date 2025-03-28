0e9210e43c6f76944e1cdadbc115b3c0b8eec487
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-28 09:34:13 +0200

    add missing provider to user rating module

--- src/movies-users-rating/movies-users-rating.module.ts
+++ src/movies-users-rating/movies-users-rating.module.ts
@@ -8,10 +8,11 @@ import { MoviesService } from 'src/movies/movies.service';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/entities/user.entity';
 import { Movie } from 'src/movies/entities/movie.entity';
+import { UserRateMovieEventHandler } from 'src/handler/event-handler/user-rate-movie..handler';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MoviesUsersRating, User, Movie])],
   controllers: [MoviesUsersRatingController],
-  providers: [MoviesUsersRatingService, JwtService, UsersService, MoviesService],
+  providers: [MoviesUsersRatingService, JwtService, UsersService, MoviesService, UserRateMovieEventHandler],
 })
 export class MoviesUsersRatingModule {}
