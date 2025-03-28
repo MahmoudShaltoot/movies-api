80febfe880b7a80a2a972a3130e76f2a05975fe9
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-28 04:39:01 +0200

    fix admin or owner guard

--- src/guards/admin-or-owner.guard.spec.ts
+++ src/guards/admin-or-owner.guard.spec.ts
@@ -1,25 +1,27 @@
+import { JwtService } from "@nestjs/jwt";
 import { UsersService } from "../users/users.service";
 import { AdminOrOwnerGuard } from "./admin-or-owner.guard";
 import { Test, TestingModule } from "@nestjs/testing";
 
 describe('AdminOrOwnerGuard', () => {
   let guard: AdminOrOwnerGuard;
-  let usersService: UsersService;
+  let jwtService: JwtService;
 
-  // Mock UsersService
-  const mockUsersService = {
+  // Mock JwtService
+  const mockJwtService = {
+    verify: jest.fn(),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         AdminOrOwnerGuard,
-        { provide: UsersService, useValue: mockUsersService },
+        { provide: UsersService, useValue: mockJwtService },
       ],
     }).compile();
 
     guard = module.get<AdminOrOwnerGuard>(AdminOrOwnerGuard);
-    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
