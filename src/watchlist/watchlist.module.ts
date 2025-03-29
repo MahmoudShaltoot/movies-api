3beef8a36df3db411125e6c3176691679991fcaa
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-03-29 16:35:44 +0200

    implement watchlist service actions

--- src/watchlist/watchlist.module.ts
+++ src/watchlist/watchlist.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WatchlistService } from './watchlist.service';
 import { WatchlistController } from './watchlist.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Movie } from 'src/movies/entities/movie.entity';
+import { Watchlist } from './entities/watchlist.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Watchlist, Movie, User])],
   controllers: [WatchlistController],
-  providers: [WatchlistService],
+  providers: [WatchlistService, JwtService],
 })
 export class WatchlistModule {}
--- src/watchlist/watchlist.service.ts
+++ src/watchlist/watchlist.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateWatchlistDto } from './dto/create-watchlist.dto';
+import { ConflictException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateWatchlistDto } from './dto/update-watchlist.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Watchlist } from './entities/watchlist.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Movie } from 'src/movies/entities/movie.entity';
 
 @Injectable()
 export class WatchlistService {
-  create(createWatchlistDto: CreateWatchlistDto) {
-    return 'This action adds a new watchlist';
+  constructor(@InjectRepository(Watchlist)
+  private watchlistRepository: Repository<Watchlist>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Movie)
+    private movieRepository: Repository<Movie>,
+  ) { }
+
+  async addToWatchlist(userId: number, movieId: number): Promise<any> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    const movie = await this.movieRepository.findOne({ where: { id: movieId } });
+    if (!user || !movie) {
+      throw new NotFoundException('User or Movie not found');
+    }
+    delete user?.password;
+
+    const existingWatchlist = await this.watchlistRepository.findOne({
+      where: { user: { id: userId }, movie: { id: movieId } },
+    });
+    if (existingWatchlist) {
+      throw new ConflictException('This movie is already in your watchlist');
+    }
+
+    const watchlistItem = this.watchlistRepository.create({ user, movie });
+    return this.watchlistRepository.save(watchlistItem);
   }
 
-  findAll() {
-    return `This action returns all watchlist`;
+  async findAll(userId: number, page: number, limit: number) {
+    console.log(userId, page, limit);
+
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['watchlists', 'watchlists.movie']
+    });
+    if (!user) {
+      throw new NotFoundException('User or Movie not found');
+    }
+
+    return user.watchlists.map((watchlist) => watchlist.movie);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} watchlist`;
-  }
+  async remove(userId: number, movieId: number) {
+    const watchlist = await this.watchlistRepository.findOneBy({ user: { id: userId }, movie: { id: movieId } });
+    if (!watchlist) {
+      throw new NotFoundException('Watchlist item not found');
+    }
 
-  update(id: number, updateWatchlistDto: UpdateWatchlistDto) {
-    return `This action updates a #${id} watchlist`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} watchlist`;
+    return this.watchlistRepository.remove(watchlist);
   }
 }
