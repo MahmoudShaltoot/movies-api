037731ed7d221809c8d50e72708dad2d323adbe4
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-04-04 14:26:52 +0200

    add guards unit tests

--- src/guards/admin-or-owner.guard.spec.ts
+++ src/guards/admin-or-owner.guard.spec.ts
@@ -1,32 +1,81 @@
-import { JwtService } from "@nestjs/jwt";
-import { UsersService } from "../users/users.service";
-import { AdminOrOwnerGuard } from "./admin-or-owner.guard";
-import { Test, TestingModule } from "@nestjs/testing";
+import { Test, TestingModule } from '@nestjs/testing';
+import { AdminOrOwnerGuard } from './admin-or-owner.guard';
+import { ExecutionContext, ForbiddenException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AdminOrOwnerGuard', () => {
   let guard: AdminOrOwnerGuard;
   let jwtService: JwtService;
-
-  // Mock JwtService
-  const mockJwtService = {
-    verify: jest.fn(),
-  };
+  let context: ExecutionContext;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminOrOwnerGuard, JwtService,
-        { 
-          provide: UsersService,
-          useValue: mockJwtService
-        },
-      ],
+      providers: [AdminOrOwnerGuard, JwtService],
     }).compile();
 
     guard = module.get<AdminOrOwnerGuard>(AdminOrOwnerGuard);
     jwtService = module.get<JwtService>(JwtService);
+    context = {
+      switchToHttp: () => ({
+        getRequest: () =>
+        ({
+          params: { id: 'user-id' },
+          headers: { authorization: 'Bearer token' },
+          user: { id: 'user-id', isAdmin: true }
+        }),
+      }),
+    } as unknown as ExecutionContext;
   });
 
-  it('should be defined', () => {
-    expect(guard).toBeDefined();
+  it('should return true if user is admin', async () => {
+    jest.spyOn(jwtService, 'verify').mockImplementation(() => {
+      return { id: '', isAdmin: true };
+    });
+
+    const result = await guard.canActivate(context);
+    expect(result).toBe(true);
+  });
+
+  it('should return true if user is owner', async () => {
+    jest.spyOn(jwtService, 'verify').mockImplementation(() => {
+      return { id: 'user-id', isAdmin: false };
+    });
+
+    context = {
+      switchToHttp: () => ({
+        getRequest: () =>
+        ({
+          params: { id: 'user-id' },
+          headers: { authorization: 'Bearer token' },
+          user: { id: 'user-id', isAdmin: false }
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    const result = await guard.canActivate(context);
+    expect(result).toBe(true);
+  });
+
+  it('should return false if user is not admin or owner', async () => {
+    jest.spyOn(jwtService, 'verify').mockImplementation(() => {
+      return { id: '', isAdmin: false };
+    });
+
+    context = {
+      switchToHttp: () => ({
+        getRequest: () =>
+        ({
+          params: { id: 'user-id' },
+          headers: { authorization: 'Bearer token' },
+          user: { id: '', isAdmin: false }
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    try {
+      await guard.canActivate(context);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ForbiddenException);
+    }
   });
 });
--- src/guards/auth.guard.spec.ts
+++ src/guards/auth.guard.spec.ts
@@ -1,29 +1,61 @@
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
   let jwtService: JwtService;
-
-  // Mock JwtService
-  const mockJwtService = {
-    verify: jest.fn(),
-  };
+  let context: ExecutionContext;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthGuard,
-        { provide: JwtService, useValue: mockJwtService },
-      ],
+      providers: [AuthGuard, JwtService],
     }).compile();
 
     guard = module.get<AuthGuard>(AuthGuard);
     jwtService = module.get<JwtService>(JwtService);
+    context = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          headers: { authorization: 'Bearer token' },
+        }),
+      }),
+    } as unknown as ExecutionContext;
   });
 
-  it('should be defined', () => {
-    expect(guard).toBeDefined();
+  it('should return true if token is valid', async () => {
+    jest.spyOn(jwtService, 'verify').mockImplementation(() => {
+      return { id: 'user-id' };
+    });
+
+    const result = await guard.canActivate(context);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false if token is invalid', async () => {
+    jest.spyOn(jwtService, 'verify').mockImplementation(() => {
+      throw new Error('Invalid token');
+    });
+    try {
+      await guard.canActivate(context);
+    } catch (e) {
+      expect(e.message).toBe('Invalid token');
+    }
+  });
+
+  it('should return false if token is missing', async () => {
+    try {
+      await guard.canActivate({
+        switchToHttp: () => ({
+          getRequest: () => ({
+            headers: { authorization: null },
+          }),
+        }),
+      } as unknown as ExecutionContext);
+    } catch (e) {
+      expect(e.message).toBe('Authorization token is missing or invalid');
+    }
   });
 });
