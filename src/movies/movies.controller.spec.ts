3678a1e7b794bb274ab129187555c9a153e0be52
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-04-04 14:27:21 +0200

    add movies unit tests

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -2,34 +2,32 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 import { createMovie } from './factory/movie.factory';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Movie } from './entities/movie.entity';
-import { JwtService } from '@nestjs/jwt';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { GenresService } from '../genres/genres.service';
+import { JwtService } from '@nestjs/jwt';
 import { Genre } from '../genres/entities/genre.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('MoviesController', () => {
   let controller: MoviesController;
-  let mockMovieRepository;
+  let moviesService: MoviesService;
   let mockGenreRepository;
 
-  mockMovieRepository = {
+  const mockMoviesService = {
     create: jest.fn().mockImplementation((createMovieDto) => createMovie(createMovieDto)),
-    save: jest.fn().mockImplementation((createMovieDto) => createMovie(createMovieDto)),
-    find: jest.fn().mockResolvedValue([createMovie(), createMovie()]),
+    findAll: jest.fn().mockResolvedValue([createMovie(), createMovie()]),
     findOne: jest.fn().mockResolvedValue(createMovie()),
-    findOneBy: jest.fn().mockResolvedValue(createMovie()),
-    update: jest.fn().mockResolvedValue({ "affected": 1 }),
-    delete: jest.fn().mockResolvedValue({ "affected": 1 })
+    update: jest.fn().mockResolvedValue({ affected: 1 }),
+    remove: jest.fn().mockResolvedValue({ affected: 1 }),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MoviesController],
-      providers: [MoviesService, JwtService, GenresService,
+      providers: [MoviesService, GenresService, JwtService,
         {
-          provide: getRepositoryToken(Movie),
-          useValue: mockMovieRepository
+          provide: MoviesService,
+          useValue: mockMoviesService,
         },
         {
           provide: getRepositoryToken(Genre),
@@ -39,9 +37,36 @@ describe('MoviesController', () => {
     }).compile();
 
     controller = module.get<MoviesController>(MoviesController);
+    moviesService = module.get<MoviesService>(MoviesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call MoviesService.create when creating a movie', async () => {
+    const createMovieDto = { title: 'Inception', original_language: 'en' } as CreateMovieDto;
+    await controller.create(createMovieDto);
+    expect(moviesService.create).toHaveBeenCalledWith(createMovieDto);
+  });
+
+  it('should call MoviesService.findAll when retrieving all movies', async () => {
+    await controller.findAll({});
+    expect(moviesService.findAll).toHaveBeenCalled();
+  });
+
+  it('should call MoviesService.findOne when retrieving movie by id', async () => {
+    await controller.findOne(1);
+    expect(moviesService.findOne).toHaveBeenCalled();
+  });
+
+  it('should call MoviesService.update when updating movie by id', async () => {
+    await controller.update(1, { title: 'Updated Title' });
+    expect(moviesService.update).toHaveBeenCalled();
+  });
+
+  it('should call MoviesService.remove when removing movie by id', async () => {
+    await controller.remove(1);
+    expect(moviesService.remove).toHaveBeenCalled();
   });
 });
--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -6,9 +6,13 @@ import { Movie } from './entities/movie.entity';
 import { Repository } from 'typeorm';
 import { GenresService } from '../genres/genres.service';
 import { Genre } from '../genres/entities/genre.entity';
+import { TmdbMovie } from 'src/tmdb/interface/tmdb.interface';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  let genreService: GenresService;
+
   let mockMovieRepository;
   let mockGenreRepository;
   let repository;
@@ -33,6 +37,11 @@ describe('MoviesService', () => {
       update: jest.fn().mockResolvedValue({ "affected": 1 }),
       delete: jest.fn().mockResolvedValue({ "affected": 1 }),
       createQueryBuilder: jest.fn(() => mockQueryBuilder),
+      manager: {
+        connection: {
+          createQueryRunner: jest.fn(),
+        }
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -46,12 +55,13 @@ describe('MoviesService', () => {
         {
           provide: getRepositoryToken(Genre),
           useValue: mockGenreRepository
-        }
+        },
       ],
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
     repository = module.get<Repository<Movie>>(getRepositoryToken(Movie));
+    genreService = module.get<GenresService>(GenresService);
   });
 
   it('should be defined', () => {
@@ -185,4 +195,144 @@ describe('MoviesService', () => {
       expect(result.affected).toEqual(0);
     });
   });
+
+  describe('saveMovieMessage', () => {
+    it('should save movie message', async () => {
+      const movie: Partial<TmdbMovie> = {
+        id: 1,
+        title: 'Movie Title',
+      };
+      mockMovieRepository.findOne.mockResolvedValueOnce(null);
+
+      await service.saveMovieMessage(movie as TmdbMovie);
+
+      expect(mockMovieRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(mockMovieRepository.create).toHaveBeenCalledTimes(1);
+      expect(mockMovieRepository.save).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw conflict error if movie found', async () => {
+      const movie: Partial<TmdbMovie> = {
+        id: 1,
+        title: 'Existing movie',
+      };
+
+      try {
+        await service.saveMovieMessage(movie as TmdbMovie);
+      } catch (error) {
+        expect(error).toBeInstanceOf(ConflictException);
+      }
+    });
+  })
+
+  describe('findAll', () => {
+    it('should return a list of movies based on filters', async () => {
+      const filters = {
+        title: 'Action',
+        genre: '["Action", "Drama"]',
+        release_date: '2021-01-01',
+        sortBy: 'average_rating',
+        sortOrder: 'asc',
+        page: '1',
+        limit: '10',
+      };
+
+      const mockGenres = [1, 2];
+
+      jest.spyOn(genreService, 'findByNames').mockResolvedValue(mockGenres);
+
+      const mockQueryBuilder = {
+        andWhere: jest.fn().mockReturnThis(),
+        orderBy: jest.fn().mockReturnThis(),
+        skip: jest.fn().mockReturnThis(),
+        take: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue([{ id: 1, title: 'Movie 1' }, { id: 2, title: 'Movie 2' }]),
+      };
+
+      mockMovieRepository.createQueryBuilder.mockReturnValue(mockQueryBuilder);
+
+      const result = await service.findAll(filters);
+
+      expect(mockMovieRepository.createQueryBuilder).toHaveBeenCalledWith('movie');
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledWith('movie.title LIKE :title', { title: '%Action%' });
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledWith('movie.genre_ids && ARRAY[:...genres]::int[]', { genres: [1, 2] });
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledWith('movie.release_date = :release_date', { release_date: '2021-01-01' });
+      expect(mockQueryBuilder.orderBy).toHaveBeenCalledWith('movie.average_rating', 'ASC');
+      expect(mockQueryBuilder.skip).toHaveBeenCalledWith(0);
+      expect(mockQueryBuilder.take).toHaveBeenCalledWith(10);
+      expect(mockQueryBuilder.getMany).toHaveBeenCalled();
+
+      expect(result).toEqual([{ id: 1, title: 'Movie 1' }, { id: 2, title: 'Movie 2' }]);
+    });
+  });
+
+  describe('updateMovieRating', () => {
+    it('should update movie rating successfully', async () => {
+      const movie_id = 1;
+      const newRating = 4.5;
+
+      const movie = {
+        id: movie_id,
+        vote_count: 10,
+        average_rating: 4.2,
+      };
+
+      const queryRunner = {
+        connect: jest.fn(),
+        startTransaction: jest.fn(),
+        commitTransaction: jest.fn(),
+        rollbackTransaction: jest.fn(),
+        release: jest.fn(),
+        manager: {
+          findOne: jest.fn().mockResolvedValue(movie),
+          update: jest.fn().mockResolvedValue({ affected: 1 }),
+        },
+      };
+
+      mockMovieRepository.manager.connection.createQueryRunner.mockReturnValue(queryRunner);
+
+      await service.updateMovieRating(movie_id, newRating);
+
+      expect(queryRunner.connect).toHaveBeenCalled();
+      expect(queryRunner.startTransaction).toHaveBeenCalled();
+      expect(queryRunner.commitTransaction).toHaveBeenCalled();
+      expect(queryRunner.release).toHaveBeenCalled();
+
+      expect(queryRunner.manager.findOne).toHaveBeenCalledWith(Movie, {
+        where: { id: movie_id },
+        lock: { mode: 'pessimistic_write' },
+      });
+
+      const expectedAvgRating = (movie.average_rating * movie.vote_count + newRating) / (movie.vote_count + 1);
+      const roundedAvgRating = parseFloat(expectedAvgRating.toFixed(2));
+      expect(queryRunner.manager.update).toHaveBeenCalledWith(Movie, movie_id, {
+        vote_count: movie.vote_count + 1,
+        average_rating: roundedAvgRating,
+      });
+    });
+
+    it('should throw an error if movie is not found', async () => {
+      const movie_id = 1;
+      const newRating = 4.5;
+
+      const queryRunner = {
+        connect: jest.fn(),
+        startTransaction: jest.fn(),
+        commitTransaction: jest.fn(),
+        rollbackTransaction: jest.fn(),
+        release: jest.fn(),
+        manager: {
+          findOne: jest.fn().mockResolvedValue(null),
+        },
+      };
+
+      mockMovieRepository.manager.connection.createQueryRunner.mockReturnValue(queryRunner);
+
+      try {
+        await service.updateMovieRating(movie_id, newRating);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    })
+  })
 });
