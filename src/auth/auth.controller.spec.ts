d33923d958eccce20a91375268df0870fb03fac3
MahmoudShaltoot <mahmoudshaltoot@yahoo.com>
2025-04-04 14:10:54 +0200

    add auth unit tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -5,11 +5,17 @@ import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { createUser } from './factory/auth.factory';
+import { UnauthorizedException } from '@nestjs/common';
+
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let usersService: UsersService;
+  let authService: AuthService;
+
   const mockUserRepository = {
-    findOne: jest.fn(),
+    findOneBy: jest.fn().mockResolvedValue(createUser({ id: 1, username: 'test' })),
     save: jest.fn(),
     find: jest.fn(),
     delete: jest.fn(),
@@ -27,9 +33,46 @@ describe('AuthController', () => {
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    usersService = module.get<UsersService>(UsersService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('register', () => {
+    it('should register a user', async () => {
+      const user = { id: 1, username: 'test', full_name: 'test', password: 'P@ssw0rd' };
+      jest.spyOn(mockUserRepository, 'findOneBy').mockResolvedValue(null);
+      jest.spyOn(usersService, 'create').mockResolvedValue(createUser());
+      jest.spyOn(authService, 'login').mockImplementation(async ({ username, password }) => { return { access_token: 'mock-token' }});
+  
+      const result = await controller.register(user);
+  
+      expect(result).toBeDefined();
+      expect(result).toHaveProperty('access_token');
+    });
+  })
+
+  describe('login', () => {
+    it('should login a user', async () => {
+      const user = { id: 1, username: 'test', password: 'P@ssw0rd' };
+      jest.spyOn(authService, 'validateUser').mockResolvedValue(createUser({ ...user }));
+      jest.spyOn(authService, 'login').mockImplementation(async ({ username, password }) => { return { access_token: 'mock-token' }});
+  
+      const result = await controller.login(user);
+  
+      expect(result).toBeDefined();
+      expect(result).toHaveProperty('access_token');
+    });
+
+    it('should throw an error if the user is not found', async () => {
+      const user = { username: 'test', password: 'incorrect-password' };
+      jest.spyOn(mockUserRepository, 'findOneBy').mockResolvedValue(null);
+      jest.spyOn(authService, 'validateUser').mockImplementation(async (username, password) => null)
+
+      await expect(controller.login(user)).rejects.toThrow(UnauthorizedException);
+    });
+  })
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { UsersModule } from '../users/users.module';
-import { ConfigService } from '@nestjs/config';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
-import { DataSource } from 'typeorm';
 import { UsersService } from '../users/users.service';
+import { createUser } from './factory/auth.factory';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
-  const mockUsersRepository = {}
+  let usersService: UsersService;
+
+  const mockUsersRepository = {
+    findOneBy: jest.fn().mockResolvedValue({ id: 1, username: 'test', password: 'P@ssw0rd' }),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -29,9 +32,70 @@ describe('AuthService', () => {
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('validateUser', () => {
+    it('should validate a user', async () => {
+      const cred = { username: 'test', password: 'P@ssw0rd' };
+      const user = createUser({ ...cred });
+      jest.spyOn(usersService, 'findOneby').mockResolvedValue(user);
+
+      const result = await service.validateUser(cred.username, cred.password);
+
+      expect(result).toBeDefined();
+      expect(result).toEqual(user);
+    });
+
+    it('should throw an error if the user is not found', async () => {
+      const cred = { username: 'not-found', password: 'incorrect-password' };
+      jest.spyOn(usersService, 'findOneby').mockResolvedValue(null);
+
+      await expect(service.validateUser(cred.username, cred.password)).rejects.toThrow(NotFoundException);
+    })
+
+    it('should throw an error if the password is incorrect', async () => {
+      const cred = { username: 'test', password: 'incorrect-password' };
+      const user = createUser({ password: 'P@ssw0rd' });
+      jest.spyOn(usersService, 'findOneby').mockResolvedValue(user);
+
+      await expect(service.validateUser(cred.username, cred.password)).rejects.toThrow(BadRequestException);
+    })
+  })
+
+  describe('login', () => {
+    it('should login a user', async () => {
+      const user = createUser();
+
+      const result = await service.login(user);
+      
+      expect(result).toBeDefined();
+      expect(result).toHaveProperty('access_token');
+    });
+  })
+
+  describe('register', () => {
+    it('should register a user', async () => {
+      const user = { id: 1, username: 'test', full_name: 'test', password: 'P@ssw0rd' };
+      jest.spyOn(mockUsersRepository, 'findOneBy').mockResolvedValue(null);
+      jest.spyOn(usersService, 'create').mockResolvedValue(createUser());
+      jest.spyOn(service, 'login').mockImplementation(async ({ username, password }) => { return { access_token: 'mock-token' }});
+  
+      const result = await service.register(user);
+  
+      expect(result).toBeDefined();
+      expect(result).toHaveProperty('access_token');
+    });
+
+    it('should throw an error if the user already exists', async () => {
+      const user = { id: 1, username: 'existing-user', full_name: 'test', password: 'P@ssw0rd' };
+      jest.spyOn(mockUsersRepository, 'findOneBy').mockResolvedValue(createUser());
+
+      await expect(service.register(user)).rejects.toThrow(BadRequestException);
+    });
+  })
 });
